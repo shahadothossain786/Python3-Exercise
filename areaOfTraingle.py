3eafceac0d2c5af647f6866f3f7efe922a6b9926
sh-rahat779 <hrahat779@gmail.com>
2019-05-20 06:08:04 -0600

    Area Of A Traingle

---
+++ areaOfTraingle.py
@@ -0,0 +1,8 @@
+#input hight of a traingle 
+#input base of a traingle
+#area of a traingle is 0.5*base*hight
+
+hight = float(input("Give a Hight Of a Traingle : "))
+base = float(input("Give a Base Of A Traingle : "))
+area = 0.5*base*hight
+print("Area of a Traingle : ",area)
