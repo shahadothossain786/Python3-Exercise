cc2d73c9ed139860df43e6a2a29ded1c59cea5c9
sh-rahat779 <hrahat779@gmail.com>
2019-05-20 07:29:16 -0600

    swape variable

--- swapeVarible.py
+++ swapeVarible.py
@@ -1,9 +1,14 @@
 #swape variable
 #create input variables and swape each other
+
 var1 = int(input("Enter An Integer Value variable 1 : "))
 var2 = int(input("Enter An Integer Value varriable 2 : "))
+
+#swapping variable
+
 swape = var1
 var1 = var2
 var2 = swape
+
 print("The Value Of Variable 1 after Swapping : {}".format(var1))
 print("The Value Of Variable 2 after Swapping : {}".format(var2))
